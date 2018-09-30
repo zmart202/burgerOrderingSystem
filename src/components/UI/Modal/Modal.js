990dc9a5cded4e83ca16ee634b6584c1c969f821
Zack Martin <zack@Zacks-MacBook-Air.local>
2018-09-30 10:47:09 -0400

    finished up the ability to put together a burger and the pricing system

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import classes from "./Modal.css";
+import Backdrop from "../Backdrop/Backdrop";
+import Aux from "../../../hoc/aux";
+
+const modal = props => {
+  return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0"
+        }}
+        className={classes.Modal}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
+
+export default modal;
